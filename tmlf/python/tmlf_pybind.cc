4a2dca41af5159d81372106ad42b53b82dd4b0d9
shunting <shunting314@gmail.com>
2020-11-25 14:03:24 -0800

    setup glog

--- tmlf/python/tmlf_pybind.cc
+++ tmlf/python/tmlf_pybind.cc
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <string>
 #include <assert.h>
+#include <glog/logging.h>
 
 namespace py = pybind11;
 
@@ -9,11 +10,12 @@ std::string ping(void) {
 }
 
 void run_net(const std::string& net_proto_ser) {
-  // LOG(FATAL) << "ni"; // TODO
+  LOG(ERROR) << "ni";
   assert(false);
 }
 
 PYBIND11_MODULE(tmlf_pybind, m) {
+  google::InitGoogleLogging("tmlf_pybind");
   m.def("ping", &ping);
   m.def("run_net", &run_net);
 }
