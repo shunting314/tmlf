685a915fe12de28867e470dea59351efc5da813a
shunting <shunting314@gmail.com>
2020-11-25 17:00:56 -0800

    implement xavier fill

--- tmlf/core/Tensor.h
+++ tmlf/core/Tensor.h
@@ -13,6 +13,9 @@ typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Ma
 class Tensor {
  public:
   explicit Tensor(int64_t w, int64_t h);
+
+  // TODO maybe it's better to not expose MatXf at all
+  MatXf& mat() { return *mat_; }
  private:
   std::shared_ptr<MatXf> mat_;
   friend std::ostream& operator<<(std::ostream& os, const Tensor& tensor);
--- tmlf/ops/xavier_fill_op.cc
+++ tmlf/ops/xavier_fill_op.cc
@@ -1,8 +1,15 @@
 #include "tmlf/core/Operator.h"
 #include <glog/logging.h>
 #include "tmlf/core/Tensor.h"
+#include <random>
 
 using namespace tmlf;
+
+static float uniform_rand(float minv, float maxv) {
+  static std::default_random_engine generator;
+  std::uniform_real_distribution<float> distribution(minv, maxv);
+  return distribution(generator);
+}
 
 class XavierFillOp : public Operator {
  public:
@@ -12,8 +19,14 @@ class XavierFillOp : public Operator {
     auto shape = arg_to_ints(getarg("shape"));
     assert(shape.size() == 2);
     Tensor out(shape[0], shape[1]);
+    auto dim_in = shape[0];
+    auto scale = sqrt(3 / dim_in);
+    for (int i = 0; i < shape[0]; ++i) {
+      for (int j = 0; j < shape[1]; ++j) {
+        out.mat()(i, j) = uniform_rand(-scale, scale);
+      }
+    }
     ws_->add_tensor(op_proto_.out_tensors()[0], out);
-    LOG(FATAL) << "\n" << ws_->get_tensor(op_proto_.out_tensors()[0]);
   }
 };
 
